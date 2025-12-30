3750506df0b3d6fbdb08ea6c494812fe64545cb8
Seb James <sebj_coder3@pm.me>
2025-12-30 16:07:21 +0000

    Adds a str() function for TextFeatures

--- mplot/TextFeatures.h
+++ mplot/TextFeatures.h
@@ -6,6 +6,9 @@
 
 namespace mplot
 {
+    struct TextFeatures;
+    std::ostream& operator<< (std::ostream&, const TextFeatures&);
+
     // A way to bundle up font size, colour, etc into a single object. Constructors chosen for max convenience.
     struct TextFeatures
     {
@@ -36,6 +39,67 @@ namespace mplot
             this->centre_horz = _centre_horz;
         }
 
+        std::string str() const
+        {
+            std::string s = {};
+            switch (this->font) {
+            case mplot::VisualFont::DVSans:
+                s += "DejaVuSans.ttf (DejaVu sans-serif) ";
+                break;
+            case mplot::VisualFont::DVSansItalic:
+                s += "DejaVuSans-Oblique.ttf (DejaVu sans-serif italic) ";
+                break;
+            case mplot::VisualFont::DVSansBold:
+                s += "DejaVuSans-Bold.ttf (DejaVu sans-serif bold) ";
+                break;
+            case mplot::VisualFont::DVSansBoldItalic:
+                s += "DejaVuSans-BoldOblique.ttf (DejaVu sans-serif bold italic) ";
+                break;
+            case mplot::VisualFont::Vera:
+                s += "Vera.ttf (Vera sans-serif) ";
+                break;
+            case mplot::VisualFont::VeraItalic:
+                s += "VeraIt.ttf (Vera sans-serif italic) ";
+                break;
+            case mplot::VisualFont::VeraBold:
+                s += "VeraBd.ttf (Vera sans-serif bold) ";
+                break;
+            case mplot::VisualFont::VeraBoldItalic:
+                s += "VeraBI.ttf (Vera sans-serif bold italic) ";
+                break;
+            case mplot::VisualFont::VeraMono:
+                s += "VeraMono.ttf (Vera sans-serif monospace) ";
+                break;
+            case mplot::VisualFont::VeraMonoItalic:
+                s += "VeraMoIt.ttf (Vera sans-serif monospace italic) ";
+                break;
+            case mplot::VisualFont::VeraMonoBold:
+                s += "VeraMoBD.ttf (Vera sans-serif monospace bold) ";
+                break;
+            case mplot::VisualFont::VeraMonoBoldItalic:
+                s += "VeraMoBI.ttf (Vera sans-serif monospace bold italic) ";
+                break;
+            case mplot::VisualFont::VeraSerif:
+                s += "VeraSe.ttf (Vera serif) ";
+                break;
+            case mplot::VisualFont::VeraSerifBold:
+                s += "VeraSeBd.ttf (Vera serif bold) ";
+                break;
+            default:
+                s += "Unknown";
+                break;
+            }
+
+            s += "size " + std::to_string (fontsize);
+            s += " res " + std::to_string (fontres);
+            s += " colour rgb(" + std::to_string (colour[0]) + "," + std::to_string (colour[0]) + "," + std::to_string (colour[0]) + ")";
+            if (this->centre_horz == true) {
+                s += " horizontally centred";
+            }
+
+            return s;
+        }
+
         //! The size for the font
         float fontsize = 0.1f;
         //! The pixel resolution for the font textures
@@ -49,5 +113,11 @@ namespace mplot
 
         // Maybe also things like rotate, centre_vert, etc
     };
+
+    std::ostream& operator<< (std::ostream& os, const TextFeatures& tf)
+    {
+        os << tf.str();
+        return os;
+    }
 
 } // namespace
